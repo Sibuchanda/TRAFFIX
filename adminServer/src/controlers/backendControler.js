f5dae49ebf110f26cb82a8b861beaa6c18e63f53
Sibuchanda <sibuchanda457@gmail.com>
2025-12-07 00:59:50 +0530

    All backend CRUD operations are implemented

--- adminServer/src/controlers/backendControler.js
+++ adminServer/src/controlers/backendControler.js
@@ -29,7 +29,87 @@ export const addBackend = async (req, res) => {
     console.error("Add Backend Error:", err);
     return res.status(500).json({
       success: false,
-      message: "Server error"
+      message: "Server error while adding backends into database"
     });
   }
 };
+
+// ========== Get all backend servers =========
+export const getBackends = async (req, res) => {
+  try {
+    const backends = await BackendServer.find().sort({ createdAt: -1 });
+    return res.json({
+      success: true,
+      backends
+    });
+
+  } catch (err) {
+    console.error("Get Backends Error:", err);
+    return res.status(500).json({
+      success: false,
+      message: "Server error while fetching all backends"
+    });
+  }
+};
+
+// ========== Update a backend server =========
+export const updateBackend = async (req, res) => {
+  try {
+    const { name, url } = req.body;
+    if(!name || !url){
+      return res.status(400).json({success: false, message: "Name and URL fields are required"})
+    }
+    const backend = await BackendServer.findById(req.params.id);
+    if (!backend) {
+      return res.status(404).json({
+        success: false,
+        message: "Backend server not found"
+      });
+    }
+
+    backend.name = name || backend.name;
+    backend.url = url || backend.url;
+
+    await backend.save();
+
+    return res.json({
+      success: true,
+      message: "Backend updated successfully",
+      backend
+    });
+
+  } catch (err) {
+    console.error("Update Backend Error:", err);
+    return res.status(500).json({
+      success: false,
+      message: "Server error while updating backend servers"
+    });
+  }
+};
+
+// ========== Delete a backend server =========
+export const deleteBackend = async (req, res) => {
+  try {
+    const backend = await BackendServer.findById(req.params.id);
+    if (!backend) {
+      return res.status(404).json({
+        success: false,
+        message: "Backend server not found"
+      });
+    }
+
+    await BackendServer.findByIdAndDelete(req.params.id);
+    return res.json({
+      success: true,
+      message: "Backend removed successfully"
+    });
+
+  } catch (err) {
+    console.error("Delete Backend Error:", err);
+    return res.status(500).json({
+      success: false,
+      message: "Server error while deleting servers from the database"
+    });
+  }
+};
+
--- adminServer/src/routes/backendRoutes.js
+++ adminServer/src/routes/backendRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js'
-import { addBackend } from '../controlers/backendControler.js';
+import { addBackend, deleteBackend, getBackends, updateBackend } from '../controlers/backendControler.js';
 
 const router = express.Router();
 
 router.post('/add', authMiddleware, addBackend);
+router.get('/all', authMiddleware, getBackends);
+router.put('/:id', authMiddleware, updateBackend);
+router.delete('/:id', authMiddleware, deleteBackend);
 
 
 
